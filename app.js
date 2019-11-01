e273f5d7bac65fbd0b3160e94a24014e9a191e9f
Ethan Miller <Ethan.Miller@improving.com>
2019-11-01 09:10:05 -0500

    connect to db and modify form view

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const mysql = require('mysql');
+
+// Database connection
+let db = mysql.createConnection({
+  host: 'localhost',
+  user: 'manager',
+  password: 'excellence',
+  database: 'inventory'
+})
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 // Init App
 const app = express();
@@ -15,8 +29,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Home Route
 app.get('/', function(req, res){
-    res.render('index');
+  let sql = 'select * from products';
+  let query = db.query(sql, (err, products) => {
+    if(err) throw err;
+    console.log(products);
+    res.render('index', {
+      products: products
+    });
   });
+});
 
 app.get('/form', function(req, res){
     res.render('addProduct');
@@ -24,6 +45,18 @@ app.get('/form', function(req, res){
 
 app.get('/product', function(req, res){
     res.render('product');
+});
+
+// Get contents of products table
+app.get('/getproducts', function(req, res){
+  let sql = 'select * from products';
+  let query = db.query(sql, (err, products) => {
+    if(err) throw err;
+    console.log(products);
+    res.render('index', {
+      products: products
+    });
+  });
 });
 
 
